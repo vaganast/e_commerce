d42906f3955322f6c26b4be020c9b3470fb6bc1d
vaganast <71605687+vaganast@users.noreply.github.com>
2022-02-16 22:05:04 +0200

    cartitem component
    appearing the images of the products that are inside the cart , quantity and add buttons + - remove of the items.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Container, Typography, Button, Grid } from '@material-ui/core'
+import React from 'react';
+import { Container, Typography, Button, Grid } from '@material-ui/core';
 
-import useStyles from './styles'
+import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
     const classes = useStyles();
@@ -15,7 +16,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id} >
-                        <div>{item.name}</div>
+                        <CartItem item={item} />
                     </Grid>
                 ))}
             </Grid>
@@ -36,7 +37,7 @@ const Cart = ({ cart }) => {
   return (
     <Container>
         <div className={classes.toolbar} />
-        <Typography className={classes.title} variant="h3"> Your Shopping Cart </Typography>
+        <Typography className={classes.title} variant="h3" gutterBottom> Your Shopping Cart </Typography>
         { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   )
