08675bd43f96432182be82a60f6dba7461bef98c
vaganast <71605687+vaganast@users.noreply.github.com>
2022-02-20 20:04:33 +0200

    cart buttons
    functionality at buttons + - empty cart
    props passing app->cart->cartItem

--- src/App.js
+++ src/App.js
@@ -16,14 +16,32 @@ const App = () => {
   const fetchCart = async () => {
       //const cart = await commerce.cart.retrieve();
      //setCart(cart);
-     setCart(await commerce.cart.retrieve())
+     setCart(await commerce.cart.retrieve());
   }
 
   const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
-        
-        setCart(item.cart);
+        const { cart } = await commerce.cart.add(productId, quantity);
+    
+        setCart(cart)
   }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+      const { cart } = await commerce.cart.update(productId, { quantity });
+
+      setCart(cart)
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+      const { cart } = await commerce.cart.remove(productId);
+
+      setCart(cart)
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart)
+}
 
   useEffect(() => {
       fetchProducts();
@@ -40,11 +58,14 @@ const App = () => {
             <Navbar totalItems={cart.total_items} />
             <Routes>
                 <Route path='/' element={<Products products = {products} onAddToCart = {handleAddToCart}/>} />           
-                <Route path='/cart' element={<Cart cart={cart} />} />  
+                <Route path='/cart' element={<Cart cart={cart}
+                  handleUpdateCartQty = {handleUpdateCartQty}
+                  handleRemoveFromCart = {handleRemoveFromCart}
+                  handleEmptyCart = {handleEmptyCart}/>} />  
             </Routes>
         </div>
       </Router>
   );
-}
+};
 
 export default App;
