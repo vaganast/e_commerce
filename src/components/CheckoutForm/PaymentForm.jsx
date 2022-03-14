f044d0124e6ed203a122af479a4e3f9e615218bf
vaganast <71605687+vaganast@users.noreply.github.com>
2022-03-14 22:02:54 +0200

    Review
    Review of order

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Typography, Button, Divider } from '@material-ui/core'
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 
-const PaymentForm = () => {
+import Review from './Review'
+
+const PaymentForm = ({ checkoutToken }) => {
   return (
-    <div>PaymentForm</div>
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
   )
 }
 
